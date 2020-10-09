35f2e883ee446db22c18a3cf36d52c72eeb54eee
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2020-10-09 20:24:27 +0900

    Display 3×3matrix

---
+++ gaussian_elimination.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main (){
+  int matrix[3][3];
+
+  printf("a11 a12 a13   x1   b1\na21 a22 a23 * x2 = b2\na31 a32 a33   x3   b3\n");
+
+  printf("a%d%d:",I, J);
+  scanf("&matrix[i][j]
+}
