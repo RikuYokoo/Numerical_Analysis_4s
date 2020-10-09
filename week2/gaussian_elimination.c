5ffc8699665d87bd9de53cdeecc9697c95adfc10
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2020-10-10 00:55:52 +0900

    Add input b matrix

--- week2/gaussian_elimination.c
+++ week2/gaussian_elimination.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 
 int main (){
-  int matrix[3][3];
+  int Smatrix[3][3];
+  int matrix[3][1];
   int a[3][3];
-  int I = 1, J = 1, i = 0, j = 0;
+  int b[3][1];
+  int I = 1, J = 1, i = 0, j = 0, brow = 1;
 
   printf("a11 a12 a13   x1   b1\na21 a22 a23 * x2 = b2\na31 a32 a33   x3   b3\n");
-
+/*a11~a33までの入力　始まり*/
   for(int R = 0;R < 9;R++){
     printf("a%d%d:", I, J);
-    scanf("%d",&matrix[i][j]);
-    while(matrix[i][j] == '\n')
-      scanf("%d",&matrix[i][j]);
+    scanf("%d",&Smatrix[i][j]);
+    while(Smatrix[i][j] == '\n')
+      scanf("%d",&Smatrix[i][j]);
     J++;
     if(j == 2){
       j = 0;
@@ -24,19 +26,30 @@ int main (){
     j++;
     //if(i >= 2 && j == 2) break;
   }
+  //ここまで
+  //b input start
+  for(int i = 0;i < 3;i++){
+    printf("b%d1 : ", brow);
+    scanf("%d", &matrix[i][0]);
+    while(matrix[i][0] == '\n')
+      scanf("%d", &matrix[i][0]);
+    brow++;
+  }
+
   for(i = 0;i < 3;i++){
-    a[i][0] = matrix[i][0];
-    a[i][1] = matrix[i][1];
-    a[i][2] = matrix[i][2];
+    a[i][0] = Smatrix[i][0];
+    a[i][1] = Smatrix[i][1];
+    a[i][2] = Smatrix[i][2];
   }
   printf("%d\n", a[1][1]);
+  //a output start
     i = 0;
     j = 0;
     I = 1;
     J = 1;
   for(int r = 0;r < 9;r++){
     //i = 0, j = 0, I = 1, J = 1;
-    printf("a%d%d : %d", I, J, matrix[i][j]);
+    printf("a%d%d : %d", I, J, Smatrix[i][j]);
     J++;
     if(j == 2){
       j = 0;
@@ -47,6 +60,12 @@ int main (){
       I++;
     }
     j++;
+  }
+  printf("\n");
+  //b output start
+  brow = 1;
+  for(i = 0;i < 3;i++){
+    printf("b%d1 : %d ", brow++, matrix[i][0]);
   }
   printf("\n");
   return 0;
