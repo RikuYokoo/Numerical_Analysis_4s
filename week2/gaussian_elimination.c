69c7574dca87fa8eb25eb826c13faf8befdd23b8
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2020-10-10 23:48:57 +0900

    Finish gaussian elimination

--- week2/gaussian_elimination.c
+++ week2/gaussian_elimination.c
@@ -14,14 +14,15 @@ int main (){
   double for_cal1row[4];
   double for_cal2row[4];
   double for_cal3row[4];
-  int I = 1, J = 1, i = 0, j = 0, brow = 1, a11, a22, a33, x1, x2, x3;//I,J:aの添字  i,j 配列の添字
+  int I = 1, J = 1, i = 0, j = 0, brow = 1;//I,J:aの添字  i,j 配列の添字
+  double a11, a22, a33, x1, x2, x3;
 
   printf("a11 a12 a13   x1   b1\na21 a22 a23 * x2 = b2\na31 a32 a33   x3   b3\n");//式を表示
 /*a11~a33までの入力　始まり*/
   for(int R = 0;R < 9;R++){
     printf("a%d%d:", I, J);
     scanf("%d",&Smatrix[i][j]);
-    printf("a%d%d\n", i, j);
+//    printf("a%d%d\n", i, j);
     while(Smatrix[i][j] == '\n')
       scanf("%d",&Smatrix[i][j]);
     J++;
@@ -44,7 +45,7 @@ int main (){
       scanf("%d", &matrix[i][0]);
     brow++;
   }
-//aに代入
+//aに代入　いらない
   for(i = 0;i < 3;i++){
     a[i][0] = Smatrix[i][0];
     a[i][1] = Smatrix[i][1];
@@ -74,7 +75,7 @@ int main (){
   }
   printf("\n");
   //計算記述開始
-  //a11,a22,a33に代入
+  //a11,a22,a33に代入 a11以外いらない
   a11 = Smatrix[0][0];
   a22 = Smatrix[1][1];
   a33 = Smatrix[2][2];
@@ -86,6 +87,9 @@ int main (){
     temporary_ab3row[i] = Smatrix[2][i];
   }
   temporary_ab1row[3] = matrix[0][0];
+  temporary_ab2row[3] = matrix[1][0];
+  temporary_ab3row[3] = matrix[2][0];
+  printf("\nb1:%f\nb2:%f\nb3:%f\n", temporary_ab1row[3], temporary_ab2row[3], temporary_ab3row[3]);
     //1~3行目を代入する。終わり
   for(i = 0;i < 4;i++){
     new_ab1row[i] =(double) temporary_ab1row[i] / a11;
@@ -104,7 +108,7 @@ int main (){
   //3:a22を1にするために2行目を/a22する。
   a22 = new_ab2row[1];
   for(i = 0;i < 4;i++){
-    new_ab2row[i] = temporary_ab2row[i] / a22;
+    new_ab2row[i] =(double) temporary_ab2row[i] / a22;
     for_cal2row[i] = new_ab2row[i];
   }
 
@@ -122,7 +126,7 @@ int main (){
   //5:a33を1にするために3行目を/a33する。
   a33 = new_ab3row[2];
   for(i = 0;i < 4;i++){
-    new_ab3row[i] = temporary_ab3row[i] / a33;
+    new_ab3row[i] =(double) temporary_ab3row[i] / a33;
     for_cal3row[i] = new_ab3row[i];
   }
   //6:a13,a23を0にするために1行目-3行目*a13, 2行目-3行目*a23をする。
