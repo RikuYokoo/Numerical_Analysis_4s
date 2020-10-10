1b3b7a8caa73c3321e85445a9d42708ae95264b8
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2020-10-10 18:54:09 +0900

    Fix matrix argument

--- week2/gaussian_elimination.c
+++ week2/gaussian_elimination.c
@@ -5,17 +5,22 @@ int main (){
   int matrix[3][1];
   int a[3][3];
   int b[3][1];
-  int I = 1, J = 1, i = 0, j = 0, brow = 1;//I,J:aの添字  i,j 配列の添字
+  double temporary_ab1row[4];
+  double temporary_ab2row[4];
+  double temporary_ab3row[4];
+  int I = 1, J = 1, i = 0, j = 0, brow = 1, a11, a22, a33;//I,J:aの添字  i,j 配列の添字
 
   printf("a11 a12 a13   x1   b1\na21 a22 a23 * x2 = b2\na31 a32 a33   x3   b3\n");//式を表示
 /*a11~a33までの入力　始まり*/
   for(int R = 0;R < 9;R++){
     printf("a%d%d:", I, J);
     scanf("%d",&Smatrix[i][j]);
+    printf("a%d%d\n", i, j);
     while(Smatrix[i][j] == '\n')
       scanf("%d",&Smatrix[i][j]);
     J++;
-    if(j == 2){
+    j++;
+    if(j == 3){
       j = 0;
       i++;
     }
@@ -23,7 +28,6 @@ int main (){
       J = 1;
       I++;
     }
-    j++;
   }
   //ここまで
   //b input start
@@ -63,5 +67,26 @@ int main (){
     printf("b%d1 : %d ", brow++, matrix[i][0]);
   }
   printf("\n");
+  //計算記述開始
+  //a11,a22,a33に代入
+  a11 = Smatrix[0][0];
+  a22 = Smatrix[1][1];
+  a33 = Smatrix[2][2];
+  printf("%d, %d, %d\n", a11, a22, a33);
+  //1:a11を1にするために1行目を/a11する。
+    //1行目を代入する。
+  for(i = 0;i < 3;i++){
+    temporary_ab1row[i] = Smatrix[0][i];
+  }
+  temporary_ab1row[3] = matrix[0][0];
+  /*確認用
+     for(i = 0;i < 4;i++){
+  printf("%f\n",temporary_ab1row[i]);
+  }*/
+  //2:a21,a31を0にするために2行目-1行目*a21, 3行目-1行目*a31をする。
+  //3:a22を1にするために2行目を/a22する。
+  //4:a12,a32を0にするために1行目-2行目*a12, 3行目-2行目*a32をする。
+  //5:a33を1にするために3行目を/a33する。
+  //6:a13,a23を0にするために1行目-3行目*a13, 2行目-3行目*a23をする。
   return 0;
 }
