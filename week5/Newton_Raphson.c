fe2782609de235b30571f2d89cd975f0ea98aaf0
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2020-10-30 14:47:51 +0900

    Add the function to write the progress to a file

--- week5/Newton_Raphson.c
+++ week5/Newton_Raphson.c
@@ -12,12 +12,15 @@ double diff_x_cos(double x)
 }
 
 int main(){
+  FILE *progress;
   double x, delta_x, fx, diff_fx, eps = 0.0000000001;
   int i = 0, n = 100;
 
   printf("初期値を入力してください。\nx:");
   scanf("%lf", &x);
   
+  progress = fopen("Newton_Raphson.txt", "w");
+
   do{
     fx = x_cosx(x);
     diff_fx = diff_x_cos(x);
@@ -27,10 +30,15 @@ int main(){
     x += delta_x;
     
     printf("%d\t:%.10f\n", i+1, x);
+
+    fprintf(progress, "%.10f\t%.10f\n", x, fx);
+
     i++;
   }while(fabs(delta_x) > eps && i <= n);
 
   printf("ans = %.10f\n", x);
+
+  fclose(progress);
 
   return 0;
 }
