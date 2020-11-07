7772afd342c5afba5da7e6ed80efe8cedf73a244
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2020-11-07 23:22:09 +0900

    upload

--- week5/Newton_Raphson.c
+++ week5/Newton_Raphson.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
 
 double x_cosx(double x)
@@ -14,7 +15,8 @@ double diff_x_cos(double x)
 int main(){
   double x, delta_x, fx, diff_fx, eps = 0.0000000001;
   int i = 0, n = 100;
-
+  char str;
+  
   printf("初期値を入力してください。\nx:");
   scanf("%lf", &x);
   
@@ -28,7 +30,30 @@ int main(){
     
     printf("%d\t:%.10f\n", i+1, x);
     i++;
-  }while(fabs(delta_x) > eps && i <= n);
+
+    if(i == n){
+      printf("解が求まりませんでした。\n終了する場合は'e'を続行する場合は'c'を初期値を再入力する場合は'r'を入力してください。");
+      scanf("%c", &str);
+      while(str == '\n')
+        scanf("%c", &str);
+      if(str == 'e'){
+        printf("終了します。\n");
+        exit(0);
+      }
+      else if(str == 'r'){
+        printf("初期値をもう一度入力してください。\n");
+        printf("x:"); scanf("%lf", &x);
+        i = 0;
+        continue;
+      }else if(str == 'c'){
+        continue;
+      }else{
+        printf("終了します。\n");
+        exit(0);
+      }
+
+    }
+  }while(fabs(delta_x) > eps /*&& i<= n*/);
 
   printf("ans = %.10f\n", x);
 
