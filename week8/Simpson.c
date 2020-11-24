b50013b10e2aa851a5b140b04b34c1b889072e87
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2020-11-25 01:18:57 +0900

    Add explanatory text

--- week8/Simpson.c
+++ week8/Simpson.c
@@ -2,8 +2,9 @@
 
 double calc(double x)
 {
-  return x*x*x*x - 2*x*x*x + x*x - 3*x +1;
+  //return x*x*x*x - 2*x*x*x + x*x - 3*x +1;
   //return x*x + 2*x - 1;
+  return 1 / x;
 }
 
 int main(){
@@ -17,42 +18,44 @@ int main(){
   printf("下a:");
   scanf("%lf", &a);
   */
-  b = 3;
-  a = 0;
+  /*--積分区間:a下端,b上端--------------------------------------*/
+  //b = 3;
+  //a = 0;
+  a = 1;
+  b = 2;
 
   c = b - a;
 
+  /*--等分nを決める入力------------------------*/
   printf("積分区間は%fです.\n何等分しますか.(nは2倍されます)\n", c);
   scanf("%d", &n);
 
   n *= 2;
-  printf("%d\n", n);//deleat
 
+  /*---⊿xを決める------------------------------------------*/
   delta_x =(double) c / n;
+  printf("delta_x = %f\n", delta_x);
 
+  /*---シンプソン法の計算-----------------------------------*/
   xn = a;
   for(int i = 0; i <= n; i++){
     if(i == 0){
       y = calc(a);
-      printf("start\n");
     }else if(i == n){
       y = calc(b);
-      printf("end\n");
     }else if(i % 2 == 0){
       y = 2 * calc(xn);
-      printf("偶数\n");
     }else{
       y = 4 * calc(xn);
-      printf("奇数\n");
     }
 
     mid += y;
-    printf("%d:%f\n", i, xn);
     xn += delta_x;
   }
   
   ans = delta_x * mid / 3;
 
+  /*---結果の表示-----------------------------------------------*/
   printf("ans is %f\n", ans);
 
   return 0;
--- week8/Trapezoidal.c
+++ week8/Trapezoidal.c
@@ -1,47 +1,55 @@
 #include<stdio.h>
-#include<math.h>
 
 double calc(double x)
 {
   //return x*x*x*x - 2*x*x*x + x*x - 3*x +1;
   return 1/x;
-  //return x*x + 2*x - 1;
 }
 
 int main(){
   double y, delta_x, a, b, c, ans, mid = 0, n, xn = 0;
   
 
+  /*
   printf("積分範囲は？\n");
   printf("上b:");
   scanf("%lf", &b);
   printf("下a:");
   scanf("%lf", &a);
+  */
 
-  c = b - a;
+  /*--積分区間:a下端,b上端--------------------------------------*/
+  //a = 0;
+  //b = 3;
+  a = 1;
+  b = 2;
 
+  c = b - a;//積分範囲
+
+  /*--等分nを決める入力------------------------*/
   printf("積分区間は%fです.\n何等分しますか.\n", c);
   scanf("%lf", &n);
 
+  /*---⊿xを決める------------------------------------------*/
   delta_x = c/n;
-  printf("%f\n", delta_x);
+  printf("delta_x = %f\n", delta_x);
   xn = a;
 
+  /*---台形法の計算-------------------------*/
   for(int i = 0; i <= n; i++){
     if(i == 0){
       y = calc(a);
-      printf("start\n");
     }else if(i == n){
       y = calc(b); 
     }else{
       y = 2 * calc(xn);
     }
     mid += y;
-    printf("%d%f\n", i, mid);
     xn += delta_x;
   }
   ans = delta_x * mid / 2;
 
+  /*---結果表示------------------------------*/
   printf("ans is %f\n", ans);
 
   return 0;
